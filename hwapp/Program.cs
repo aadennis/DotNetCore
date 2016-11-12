e3a29f77ef73ef4602a20d660ec3153a19d4854c
Dennis Wells <dennis.circ@gmail.com>
2016-11-12 23:19:35 +0000

    First use of SqlLite
    Create here:
    http://zetcode.com/db/sqlitecsharp/intro/
    https://www.microsoft.com/net/core#windows
    https://blogs.msdn.microsoft.com/dotnet/2016/11/09/net-core-data-access/

---
+++ hwapp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+public class Example
+{
+
+    static void Main()
+    {
+        string cs = "Data Source=:memory:";
+
+        using (SqliteConnection con = new SqliteConnection(cs))
+        {
+            con.Open();
+            var commandText = "SELECT SQLITE_VERSION()";
+            using (SqliteCommand cmd = new SqliteCommand(commandText, con))
+            {
+                
+                var version = Convert.ToString(cmd.ExecuteScalar());
+                Console.WriteLine("SQLite versionx : {0}", version);
+            }
+            con.Close();
+        }
+    }
+}
