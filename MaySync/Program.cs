3ae94764790593748fcdd897c8e3259a213802df
aadennis <dennis.circ@gmail.com>
2016-11-28 21:43:04 +0000

    Quite unsuccessful async in console app

---
+++ MaySync/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+         DoIt();
+         //Console.Read();
+         System.Console.WriteLine("Pretty much done here");
+        }
+
+        public async static void DoIt() {
+            RunTask().GetAwaiter().GetResult();
+            //RunTask().Wait();
+            
+        }
+
+         public static async Task RunTask() {
+            Console.WriteLine("Hello World!");
+            await Task.Delay(5000);
+            System.Console.WriteLine("Hello Planet");
+
+        }
+    }
+}
