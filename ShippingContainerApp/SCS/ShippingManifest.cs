e430464224aa702d58e7207e9f64e22179fc4918
Dennis Wells <dennis.circ@gmail.com>
2016-12-10 22:35:21 +0000

    DotNetCore Xunit test

---
+++ ShippingContainerApp/SCS/ShippingManifest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ShippingContainerApp
+{
+    public class ShippingManifest
+    {
+        public int GetNumber(int number) =>
+            Newtonsoft.Json.JsonConvert.DeserializeObject<int>($"{number}");
+    }
+}
---
+++ ShippingContainerApp/SCT/ShippingManifestTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+using ShippingContainerApp;
+
+namespace ShippingContainerApp
+{
+    public class ShippingManifestTests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void ThingGetsObjectValFromNumber()
+        {
+            Assert.Equal(423, new ShippingManifest().GetNumber(42));
+        }
+    }
+}
