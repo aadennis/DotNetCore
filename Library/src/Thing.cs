3694b7036ada9cb70cfa16ae6677832533a1badd
aadennis <dennis.circ@gmail.com>
2016-12-10 14:51:15 +0000

    DotNetCore - xunit tests
    Note the folder structure - this works... I don't know right now if
    another structure works.

---
+++ Library/src/Thing.cs
@@ -0,0 +1,6 @@
+﻿namespace ClassLibrary {
+    public class Thing {
+       public int Get(int number) => 
+            Newtonsoft.Json.JsonConvert.DeserializeObject<int>($"{number}");
+    }
+}
---
+++ Library/test/Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+using ClassLibrary;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void ThingGetsObjectValFromNumber()
+        {
+            Assert.Equal(423, new Thing().Get(42));
+        }
+    }
+}
